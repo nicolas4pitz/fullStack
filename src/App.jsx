a9be43ee29b9d4e35926bfb7b6fafa21ca4bcf7f
nicolas4pitz <nicolas2004pitz@gmail.com>
2026-07-29 12:59:32 -0300

    fix: error message

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
-  const [errorMessage, setErrorMessage] = useState('some error happened...')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
